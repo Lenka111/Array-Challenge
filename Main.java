858654d23b2918ce39b737d58082829341196607
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-02-22 23:09:19 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // create an array with 5 number the user inputs
+        int [] numArray = getNumbers(5);
+        int[] sorted = sortArray(numArray);
+        printArray(sorted);
+
+
+
+    }
+    // get the numbers from the user
+    public static int[] getNumbers(int num){
+
+        int [] values = new int[num];
+        System.out.println("Enter " + num + " numbers");
+        for(int i = 0; i< values.length; i++){
+            values[i] = sc.nextInt();
+        }
+        return values;
+    }
+    public static void printArray(int[]array){
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Element " + i + " contains " + array[i]);
+        }
+
+    }
+    public static int[] sortArray(int[] array){
+        /* built in java way to sort an array
+        * int sorted[] = Arrays.copyOf(array, array.length)
+        * */
+
+        int [] sorted = new int[array.length];
+
+        for(int i = 0; i < array.length; i ++){
+            //create a copy of array and saving it into sorted array
+            sorted[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+        //sort the  array
+        while(flag){
+            flag = false;
+            for(int i= 0; i< sorted.length-1; i++){
+                if(sorted[i] < sorted[i+1]){
+                    //swap the the elements
+                    temp = sorted[i];
+                    sorted[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sorted;
+    }
+}
